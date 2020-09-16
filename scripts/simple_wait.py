c14004c1ccf89d007cfae3cc56c12d2c355de942
gjstein <gregory.j.stein@gmail.com>
2020-09-16 13:20:06 -0400

    some code formatting

--- scripts/simple_wait.py
+++ scripts/simple_wait.py
@@ -2,20 +2,17 @@ import argparse
 import random
 import time
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed',
-                        type=int,
-                        required=True)
+    parser.add_argument('--seed', type=int, required=True)
     args = parser.parse_args()
 
     print(f"Seed {args.seed}: Starting")
     random.seed(args.seed)
-    sleep_time = 1 + 1*random.random()
+    sleep_time = 1 + 1 * random.random()
     time.sleep(sleep_time)
     print(f"Seed {args.seed}: Ending")
-
-
 
 
 if __name__ == "__main__":
